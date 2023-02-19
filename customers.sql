82726b56b17eae80dd096d853cda54abeeace924
Stephen Klinck <klinck.steve.h@gmail.com>
2023-02-19 22:19:43 +0000

    Used example SQL

--- customers.sql
+++ customers.sql
@@ -1,9 +1,21 @@
-select distinct cust.id as customer_id, 
-cust.name, 
-cust.email, 
-min(ord.created_at) over (partition by ord.customer_id) as first_order_at, 
-count(ord.id) over (partition by ord.customer_id)   
-from `analytics-engineers-club.coffee_shop.customers` cust 
-left join analytics-engineers-club.coffee_shop.orders ord
-on cust.id = ord.customer_id
-order by first_order_at limit 5;
+with customer_orders as (
+  select
+     customer_id
+     , count(*) as n_orders
+     , min(created_at) as first_order_at
+
+  from `analytics-engineers-club.coffee_shop.orders` 
+  group by 1
+)
+
+select 
+  customers.id as customer_id
+  , customers.name
+  , customers.email
+  , coalesce(customer_orders.n_orders, 0) as n_orders
+  , customer_orders.first_order_at
+from `analytics-engineers-club.coffee_shop.customers` as customers
+left join  customer_orders
+  on  customers.id = customer_orders.customer_id 
+
+limit 5
